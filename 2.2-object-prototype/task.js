a8f4dbd4a5f3543858d4289ef3b0019897b6a6bc
Alex Push <lex-ll@mail.ru>
2020-04-08 12:19:23 +0400

    add task

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,14 +1,28 @@
 function getAnimalSound(animal) {
-    // код для задачи №1 писать здесь
-    // return animalSound;
+    'use strict';
+    if (!animal) return null;
+    let sound = animal.sound;
+    if (sound) return sound;
 }
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+    if (marks.length === 0) return 0;
+    let sum = 0;
+    for(let i = 0; i < marks.length; i++) {
+        sum += marks[i];
+    }
+    let average = sum / marks.length;
+    return Math.round(average);
+
 }
 
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+    const now = Date.now();
+    const date = new Date(birthday);
+    birthday = +date;
+    const diff = now - birthday;
+    const age = diff / ( 1000 * 60 * 60 * 24 * 365);
+    if (age >= 18) return true;
+    else return false;
+
 }
